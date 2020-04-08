ee92f30ac43072a38b9d48d5624d1ad6677e1cc0
nevelson <nevelson92@gmail.com>
2020-04-08 15:46:55 -0400

    Testing

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,38 +1,34 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
-  static defaultProps = { sections: [
-    {title: "Thing 1", content: "Lorem ipsum dolor sit amet."}, 
-    {title: "Thing 2", content: "This should appear and the others go away."}, 
-    {title: "Thing 3", content: "I said GO AWAY!"},
-    ] 
+  static defaultProps = {
+    sections: [
+      { title: "Thing 1", content: "Lorem ipsum dolor sit amet." },
+      { title: "Thing 2", content: "This should appear and the others go away." },
+      { title: "Thing 3", content: "I said GO AWAY!" },
+    ]
   };
 
   state = {
     pressed: null
   }
 
-  clickHandler = e => {
-    const val = e.target.id
-    this.setState({
-      pressed: val
-     }
-  )};
+  handleClick = index => {
+    this.setState({ pressed: index })
+  };
 
   render() {
-    const renderList = this.props.sections.map((ob, index) => {
-      if (parseInt(this.state.pressed) === index) {
-        return <li key={index}><button id={index} onClick={e=> {this.clickHandler(e)}}>{ob.title}</button><p>{ob.content}</p></li>;
-    } else {
-        return <li key={index}><button id={index} onClick={e=> {this.clickHandler(e)}}>{ob.title}</button></li>;
-    }
-  });
+    const { sections } = this.props;
+    const { pressed } = this.state;
     return (
-      <div>
-        <ul>
-          {renderList}
-        </ul>
-      </div>
+      <ul className="container">
+        {sections.map((section, index) => {
+          return (<li key={index} >
+            <button onClick={() => this.handleClick(index)}>{section.title}</button>
+            {(pressed === index) && <p>{section.content}</p>}
+          </li>)
+        })}
+      </ul>
     );
   }
 }
--- src/Accordion.test.js
+++ src/Accordion.test.js
@@ -1,13 +1,37 @@
-// import { shallow } from 'enzyme'
-// import toJson from 'enzyme-to-json'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Accordion from './Accordion';
 
-describe('<Accordion />', () => {
+describe(`Accordian Component`, () => {
+  it('renders without errors', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Accordion />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+
   it('renders empty li', () => {
     const tree = renderer.create(<Accordion />).toJSON();
     expect(tree).toMatchSnapshot();
+  })
+
+  it('The component renders no sections as active by default.', () => {
+    const tree = renderer.create(<Accordion sections={[{ title: "Thing 1", content: "Lorem ipsum dolor sit amet." }]} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+
+  it('The component opens a clicked section.', () => {
+    const wrapper = shallow(<Accordion />);
+    wrapper.find('button').at(1).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
+
+  it('The component only opens the last section when multiple sections have been clicked.', () => {
+    const wrapper = shallow(<Accordion />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
   })
 });
